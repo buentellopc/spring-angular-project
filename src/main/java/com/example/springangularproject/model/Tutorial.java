4350cbc85d4d5f8f5ccbb6fe32bd4e4e87e7d041
buentellopc <buentellopc@com>
2022-08-28 19:40:58 -0500

    Create spring rest api controller

--- src/main/java/com/example/springangularproject/model/Tutorial.java
+++ src/main/java/com/example/springangularproject/model/Tutorial.java
@@ -22,5 +22,43 @@ public class Tutorial {
     public Tutorial() {
     }
 
+    public Tutorial(String title, String description, boolean published) {
+        this.title = title;
+        this.description = description;
+        this.published = published;
+    }
 
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isPublished() {
+        return published;
+    }
+
+    public void setPublished(boolean published) {
+        this.published = published;
+    }
+
+    @Override
+    public String toString() {
+        return "Tutorial{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", published=" + published +
+                '}';
+    }
 }
